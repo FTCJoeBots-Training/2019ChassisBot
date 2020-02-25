b55ae9831e90776e14b6f11371291e40d0d1506e
13610 <tengsfamily@gmail.com>
2020-02-24 19:57:14 -0500

    Removed vuforia and updated strafe inputs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpSimpleMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpSimpleMecanum.java
@@ -74,7 +74,7 @@ public class teleOpSimpleMecanum extends LinearOpMode {
 
             // Calculate motor power
             power0 = forward + clockwise - right;
-            power1 = forward - clockwise + right;
+            power1 = forward - clockwise + right; 
             power2 = forward + clockwise + right;
             power3 = forward - clockwise - right;
 
